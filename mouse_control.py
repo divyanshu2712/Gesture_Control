07ec0f2def3e40066307e489ef9a04e6f69afe75
divyanshu2712 <divyanshuguptarmp790@gmail.com>
2023-10-11 16:32:45 +0530

    Updated utility and Added handtracking Module

--- mouse_control.py
+++ mouse_control.py
@@ -1,20 +1,71 @@
 import cv2
 import mediapipe as mp
-from cvzone.HandTrackingModule import HandDetector
+from gccs import HandDetector
 import time
-
+import numpy as np
+import pyautogui
 ###################################################
-wcam=1920
-hcam=1080
+wcam=640
+rf=100 #reduced frame
+hcam=480
+wscreen,hscreen=pyautogui.size()
+pyautogui.FAILSAFE=False
 ###################################################
 cap=cv2.VideoCapture(0)
 cap.set(3,wcam)
 cap.set(4,hcam)
-detector=HandDetector(detectionCon=0.75,maxHands=2)
+detector=HandDetector(detectionCon=0.5,maxHands=2)
+ptime=0
 while True:
-    _,img=cap.read()
+    success,img=cap.read()
     img=cv2.flip(img,1)
+    #Getting Hands
     hands,img=detector.findHands(img,flipType=False)
+    if len(hands)==1:
+        hand1=hands[0]
+        if hand1['type']=='Right':
+            #Getting Landmarks
+            lm_list=hand1['lmList']
+            fingers_up=detector.fingersUp(hand1)
+            # print(fingers_up)
+            #Moving Mouse
+            if fingers_up[1]==1 and fingers_up[2]==0:
+                x1,y1=lm_list[8][:2]
+                # print(x1,y1)
+                #Frame Rectangle
+                cv2.rectangle(img,(rf,rf),(wcam-rf,hcam-rf),(255,0,255),5)
+                #Convert coordinates
+                x3=np.interp(x1,(rf,wcam-rf),(0,wscreen))
+                y3=np.interp(y1,(rf,hcam-rf),(0,hscreen))
+                cv2.circle(img,(x1,y1),15,(144, 238, 144),cv2.FILLED)
+                pyautogui.moveTo(x3,y3)
+            #Clicking Mode
+            if fingers_up[1]==1 and fingers_up[2]==1:
+                #index finger
+                x1,y1=lm_list[8][:2]
+                #middle finger
+                x2,y2=lm_list[12][:2]
+                length,info,img=detector.findDistance((x1,y1),(x2,y2),img,color=(144,238,144),scale=10)
+                # cv2.circle(img,(x1,y1),15,(144, 238, 144),cv2.FILLED)
+                # cv2.circle(img,(x2,y2),15,(144, 238, 144),cv2.FILLED)
+                # print(length)            
+                if length<30:
+                    cv2.circle(img,(info[-2],info[-1]),15,(144, 238, 144),cv2.FILLED)
+                    pyautogui.leftClick()
+                    
+    
+    
+    
+    
+    
+    
+    
+    #Frame Rate
+    ctime=time.time()
+    fps=1/(ctime-ptime)
+    ptime=ctime
+    cv2.putText(img,f"FPS: {str(int(fps))}",(30,50),cv2.FONT_HERSHEY_PLAIN,3,(255,0,255),3)
+    #Displaying
     cv2.imshow("Mouse_Control",img)
     k=cv2.waitKey(1)
     if k==27:
--- utility_control.py.py
+++ utility_control.py.py
@@ -1,5 +1,5 @@
 import cv2
-from cvzone.HandTrackingModule import HandDetector
+from gccs import HandDetector
 import numpy as np
 import time
 import math
@@ -11,11 +11,11 @@ import os
 
 #########################################
 # GLOBALS & CONSTANTS
-w_cam=1080 
-h_cam=640
+w_cam=1920
+h_cam=1080
 opt=-1
 #########################################
-cap=cv2.VideoCapture(0)
+cap=cv2.VideoCapture(0) #Getting the camera '0' signify the default camera
 detector=HandDetector(detectionCon=0.75,maxHands=2,)
 cap.set(3,w_cam)#prop id for width is 3
 cap.set(4,h_cam)#prop id for height is 4 
@@ -25,41 +25,74 @@ interface = devices.Activate(
     IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
 volume = interface.QueryInterface(IAudioEndpointVolume)
 
-
 def volume_control(value):
+    '''
+    Function to control volume
+    @param:
+    value: It is a distance between finger passed to change volume accordingly 
+    '''
     vol_value=np.interp(value,[30,210],[-65.25,0])
     # print(value,vol_value)
     volume.SetMasterVolumeLevel(vol_value, None)
 
 def brightness_control(value):
+    '''
+    Function to control brightness
+    @param:
+    value: It is a distance between finger passed to change brightness accordingly 
+    '''
     new_brightness=np.interp(value,[30,210],[0,100])
     # print(value,new_brightness)
     set_brightness(new_brightness)
 
 def shutdown(value):
+    '''
+    Function to shutdown the pc when the distance between fingers become low
+    @param:
+    value: It is a distance between finger passed to it. 
+    '''
     if value<25:
-        os.system("shutdown /s /t 1")
-        # print("Shutdown")
+        # os.system("shutdown /s /t 1")
+        print("Shutdown")
 
+
+def gui_rectangle(img,bar=400):
+    '''
+    It is a function used to show a bar to showcase current level of volume or brightness.
+    @param:
+    img: It is the image matrix on which the rectangle is to be drawn
+    bar: It is value passed of the current distance between fingers
+    '''
+    bar=np.interp(bar,[30,210],[400,200])
+    cv2.rectangle(img,(30,200),(60,400),(0,255,0),3)
+    cv2.rectangle(img,(30,int(bar)),(60,400),(0,255,0),cv2.FILLED)
+#Continously getting frames 
 while True:
     success,img=cap.read()
-    img=cv2.flip(img,1)
-    hands,img=detector.findHands(img,flipType=False)
+    #img is the frame captured
+    #success is the value which tell whether read is successful or not
+    img=cv2.flip(img,1) ## Flipping the image so that there is no confusion
+    hands,img=detector.findHands(img,flipType=False) ## Getting hands
+    # Perform work if there is only a single hand in frame
     if len(hands)==1:
         hand1=hands[0]
         if hand1['type']=="Left":
-            opt=detector.fingersUp(hand1).count(1)-1
+            #If hand is left then it counts the fingers
+            opt=detector.fingersUp(hand1)[1:].count(1)
             # print(opt)
         if hand1['type']=="Right":
+            #If the hand is right then it perform the control work
             lm_list=hand1['lmList']
             if opt==1:
                 dist,info,img=detector.findDistance(lm_list[4][:2],lm_list[8][:2],img,scale=8)
                 volume_control(dist)
                 cv2.putText(img,"Volume Control",(10,100),cv2.FONT_HERSHEY_PLAIN,2,(34,129,253),4)
+                gui_rectangle(img,dist)
             if opt==2:
                 dist,info,img=detector.findDistance(lm_list[4][:2],lm_list[8][:2],img,scale=8)
                 brightness_control(dist)
                 cv2.putText(img,"Brightness Control",(10,100),cv2.FONT_HERSHEY_PLAIN,2,(34,129,253),4)
+                gui_rectangle(img,dist)
             if opt==3:
                 dist,info,img=detector.findDistance(lm_list[4][:2],lm_list[8][:2],img,scale=8)
                 shutdown(dist)

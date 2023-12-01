ce4248dc9c31f4eddf7b9f21c27f9c78fa99b79c
divyanshu2712 <divyanshuguptarmp790@gmail.com>
2023-12-01 20:06:32 +0530

    Air Canvas

--- aircanvas.py
+++ aircanvas.py
@@ -3,80 +3,204 @@ from gccs import HandDetector
 import cv2
 import time
 import os
-
+import shutil
+import img2pdf
+from datetime import datetime
+from plyer import notification
 ########################
 ########################
 drawColor=(0,0,255)
-thick=15
-erase_thick=100
+thickness=10
+thick=1
+erase_thick=thickness*10
+save_interval = 10  # Save every 10 seconds
+prev_interval=5 # Prev Button Enable after 20 sec
+next_interval=5 # Prev Button Enable after 20 sec
+download_interval=10
+last_save_time = time.time()
+last_prev_time = time.time()
+last_next_time = time.time()
+last_download_time = time.time()
 xp=0
 yp=0
 Canvas=np.full((720,1280,3),255,np.uint8)# its format is height,width, channel, dtype=unsigned int(0-255)
+num=1
+prev=1
+next=1
 ########################
 ########################
 img_folder="aircanvastop"
+right_folder="aircanvasright"
+left_folder="aircanvasleft"
 mylist=os.listdir(img_folder)
-overlaylist=[]
+rightside=os.listdir(right_folder)
+leftside=os.listdir(left_folder)
+overlayright=[]
+overlayleft=[]
+overlaytop=[]
 for impath in mylist:
     image=cv2.imread(f"{img_folder}/{impath}")
-    overlaylist.append(image)
+    overlaytop.append(image)
 
-header=overlaylist[0]
+for impath in rightside:
+    image=cv2.imread(f"{right_folder}/{impath}")
+    overlayright.append(image)
 
+for impath in leftside:
+    image=cv2.imread(f"{left_folder}/{impath}")
+    overlayleft.append(image)
+
+header=overlaytop[0]
+right=overlayright[0]
+left=overlayleft[0]
 cap=cv2.VideoCapture(0)
 cap.set(3,1280)
 cap.set(4,720)
 detector=HandDetector(detectionCon=0.85)
+shutil.rmtree("output")
+os.makedirs("output")
 while True:
     success,img=cap.read()
     #Placing Header Image
     img=cv2.flip(img,1)
     img[0:127,0:1280]=header
     Canvas[0:127,0:1280]=header
+    Canvas[127:720,1208:1280]=right
+    Canvas[127:720,0:72]=left
     #Finding hands
+    cv2.circle(Canvas,(1220,50),thickness,(0,117,252),cv2.FILLED)
+    cv2.putText(Canvas,f"Thickness",(1185,95),cv2.FONT_HERSHEY_PLAIN,1,(255,255,255),1)
     hands,img=detector.findHands(img,flipType=False,draw=False)
+    Canvas_Copy=Canvas.copy()
+
+    thickness=thick*5
+    erase_thick=thickness*10
+
     if len(hands)==1:
         hand=hands[0]
-        lmlist=hand['lmList']
-        # print(lmlist)
-        #tip of index and middle finger
-        x1,y1=lmlist[8][:2]
-        x2,y2=lmlist[12][:2]
-
-        fingers=detector.fingersUp(hand)
-        if fingers[1] and fingers[2]:
-            #Checking for click
-            xp,yp=x1,y1
-            if y1<132:
-                if 200<x1<400:
-                    header=overlaylist[0]
-                    drawColor=(0,0,255)
-                elif 450<x1<600:
-                    header=overlaylist[1]
-                    drawColor=(255,0,0)
-                elif 650<x1<800:
-                    header=overlaylist[2]
-                    drawColor=(0,255,0)
-                elif 850<x1<1000:
-                    header=overlaylist[3]
-                    drawColor=(255,255,255)
-                elif 1050<x1<1200:
-                    header=overlaylist[4]
-
-        if fingers[1]==1 and fingers[2]==0 and fingers[1:].count(1)==1:
-            cv2.circle(img,(x1,y1),15,drawColor,cv2.FILLED)
-            if xp==0 and yp==0:
+        if hand['type']=='Left':
+            thick=detector.fingersUp(hand)[1:].count(1)
+            if thick==0:
+                thick=1
+        if hand['type']=="Right":
+            lmlist=hand['lmList']
+            # print(lmlist)
+            #tip of index and middle finger
+            x1,y1=lmlist[8][:2]
+            x2,y2=lmlist[12][:2]
+            fingers=detector.fingersUp(hand)
+            if fingers[1] and fingers[2]:
+                #Checking for click
                 xp,yp=x1,y1
-            #drawing
-            #special condition for eraser
-            if drawColor==(255,255,255):
-                cv2.line(img,(xp,yp),(x1,y1),drawColor,erase_thick)
-                cv2.line(Canvas,(xp,yp),(x1,y1),drawColor,erase_thick)
-            else:    
-                cv2.line(img,(xp,yp),(x1,y1),drawColor,thick)
-                cv2.line(Canvas,(xp,yp),(x1,y1),drawColor,thick)
-    cv2.imshow("Image",img)
-    cv2.imshow("Canvas",Canvas)
+                if y1<132:
+                    if 200<x1<400:
+                        header=overlaytop[0]
+                        drawColor=(0,0,255)
+                        right=overlayright[0]
+                        left=overlayleft[0]
+                    elif 450<x1<600:
+                        header=overlaytop[1]
+                        drawColor=(255,0,0)
+                        right=overlayright[0]
+                        left=overlayleft[0]
+                    elif 650<x1<800:
+                        header=overlaytop[2]
+                        drawColor=(0,255,0)
+                        right=overlayright[0]
+                        left=overlayleft[0]
+                    elif 850<x1<1000:
+                        header=overlaytop[3]
+                        drawColor=(255,255,255)
+                        right=overlayright[0]
+                        left=overlayleft[0]
+                    elif 1050<x1<1200:
+                        #New Page Logic and Saving Previous
+                        if time.time() - last_save_time >= save_interval:
+                            last_save_time = time.time()
+                            cv2.imwrite(f'output/{num}.png', Canvas)
+                            if num==1:
+                                prev=1
+                                next=1
+                            else:
+                                prev=num
+                                next=prev+1
+                            num=num+1
+                        Canvas=np.full((720,1280,3),255,np.uint8)
+                        Canvas[0:127,0:1280]=header
+                        Canvas[127:720,1208:1280]=right
+                        Canvas[127:720,0:72]=left
+                        cv2.circle(Canvas,(1220,50),thickness,(0,117,252),cv2.FILLED)
+                        cv2.putText(Canvas,f"Thickness",(1185,95),cv2.FONT_HERSHEY_PLAIN,1,(255,255,255),1)
+                elif x1>1208:
+                    if 130<y1<200:
+                        #Previous Logic
+                        if time.time() - last_prev_time >= prev_interval:
+                            last_prev_time = time.time()
+                            if (prev+1)==num:
+                                cv2.imwrite(f"output/{num}.png",Canvas)
+                                num=num+1
+                            else:
+                                next=prev+1
+                            print(prev)
+                            if os.listdir("output")!=[]:
+                                Canvas=cv2.imread(f"output/{prev}.png")
+                            if prev>1:
+                                prev=prev-1
+                    elif 230<y1<300:
+                        #Download
+                        if time.time() - last_download_time >= download_interval:
+                            last_download_time = time.time()
+                            img_dir = 'output'
+                            pdf_dir = 'notes'
+                            img_files = [os.path.join(img_dir, f) for f in os.listdir(img_dir)]
+
+                            current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
+                            if img_files!=[]:
+                                with open(os.path.join(pdf_dir, f'{current_time}.pdf'), 'wb') as f:
+                                    f.write(img2pdf.convert(img_files))
+                                notification_title = "Download Success"
+                                notification_text = f"File saved at notes/{current_time}"
+                                notification_timeout = 10  # Notification display time in seconds
+                                icon_path = "logo/icon.ico"
+                                notification.notify( title=notification_title, message=notification_text, timeout=notification_timeout, app_icon=icon_path)
+                    elif 330<y1<400:
+                        #Rectangle
+                        right=overlayright[1]
+                elif x1<72:
+                    if 130<y1<200:
+                        #NextPage
+                        if time.time() - last_next_time >= next_interval:
+                            last_next_time = time.time()
+                            if next!=num:
+                                Canvas=cv2.imread(f"output/{next}.png")
+                            if next==num:
+                                prev=next-1
+                            else:
+                                next=next+1
+                                prev=prev+1
+                    elif 230<y1<300:
+                        #Line
+                        left=overlayleft[1]
+                    elif 330<y1<400:
+                        #Circle
+                        left=overlayleft[2]
+            if fingers[1]==1 and fingers[2]==0 and fingers[1:].count(1)==1:
+                cv2.circle(img,(x1,y1),15,drawColor,cv2.FILLED)
+                if xp==0 and yp==0:
+                    xp,yp=x1,y1
+                #drawing
+                #special condition for eraser
+                if drawColor==(255,255,255):
+                    cv2.line(img,(xp,yp),(x1,y1),drawColor,erase_thick)
+                    cv2.line(Canvas,(xp,yp),(x1,y1),drawColor,erase_thick)
+                else:    
+                    cv2.line(img,(xp,yp),(x1,y1),drawColor,thickness)
+                    cv2.line(Canvas,(xp,yp),(x1,y1),drawColor,thickness)
+                xp,yp=x1,y1
+            Canvas_Copy=Canvas.copy()
+            cv2.circle(Canvas_Copy,(x1,y1),12,(15,10,20),cv2.FILLED)
+                    
+    cv2.imshow("Canvas",Canvas_Copy)
     if cv2.waitKey(1)==27:
         break
 cv2.destroyAllWindows()
